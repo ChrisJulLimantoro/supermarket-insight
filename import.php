ee56ae55db1617e51978da115959f45a6a3a6414
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2023-12-03 19:37:12 +0700

    feat: add import template

---
+++ import.php
@@ -0,0 +1,20 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- CDN for jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+<!-- CDN for Tailwind -->
+<script src="https://cdn.tailwindcss.com/3.3.0"></script>
+<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+<!-- CDN for Tailwind Element -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/tw-elements.min.css">
+<!-- CDN for SweetAlert -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap"
+    rel="stylesheet" />
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Spinnaker&display=swap');
+*{font-family : 'Spinnaker',sans-serif !important}
+</style>
