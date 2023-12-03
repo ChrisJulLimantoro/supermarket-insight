ac699ed3efce59e42bc47b0cbf1f3623da856959
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2023-12-03 19:31:55 +0700

    feat: add connect.php

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+    require_once 'autoload.php';
+    // connection to MySql
+    $conn_sql = new PDO("mysql:host=localhost;dbname=supermarket_insight", 'root', '');
+
+    // Connection to MonggoDB
+    $conn_mongo = new MongoDB\Client();
+    $conn_mongo = $conn_mongo->Ceje->supermarket;
+
+    // Connection to Neo4j
+    $conn_neo = Laudis\Neo4j\ClientBuilder::create()
+        ->withDriver('default', 'neo4j://neo4j:password@localhost')
+        ->build();
+?>
